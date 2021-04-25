676f59dd2d3a5be138623b77d7cc3c2ce0743c8e
Barb Marques <barbara.ann.marques@gmail.com>
2021-04-25 15:28:16 -0500

    revisions to wrangle code

--- wrangle.py
+++ wrangle.py
@@ -1,22 +1,29 @@
 import pandas as pd
 
-def wrangle_curr_logs():
+def wrangle_curr_logs(df):
 
     '''
-    This function write the gzip curriculum logs to a dataframe, renames columns to 
-    user-friendly names, creates a datetime column from 'date' and 'time' and deletes
+    This function performs the following operations on the dataframe containing Codeup's
+    curriculum logs.  It renames columns to user-friendly names, drops unneccessary 
+    columns, creates a datetime column from 'date' and 'time' and deletes
     the original date and time columns, then it converts datetime to datetime format and 
     sets the column as the index for time series analysis.
     '''
     
-    # write gzip, csv file to dataframe
-    df = pd.read_csv('anonymized-curriculum-access.txt.gz', compression='gzip', header=None, sep=' ', quotechar='"', error_bad_lines=False)
-    
     #rename columns to more descriptive names
-    df = df.rename(columns = {0: "date", 1:"time", 2:"page", 3:"user", 4:"cohort", 5:"ip"})
+    df = df.rename(columns = {'name': 'cohort', 'user_id':'user', 'program_id':'program'})
     
-    # concat Date Time columns and drop original columns
-    df["datetime"] = df["date"] + ' ' + df["time"] 
+    #drop unneccessary columns
+    df = df.drop(columns = ['deleted_at','updated_at','created_at','start_date','end_date','slack','cohort_id', 'id'])
+    
+    #replace program id with names
+    df = df.replace({'program': 1}, 'web_dev')
+    df = df.replace({'program': 2}, 'staff')
+    df = df.replace({'program': 3}, 'data_sci')
+    df = df.replace({'program': 4}, 'other')
+    
+    # concat Date Time columns and drops original columns
+    df["datetime"] = df["date"] + ' '+ df["time"] 
     df.drop(columns = ['date','time'], inplace = True)
     
     #create DateTime column
@@ -25,5 +32,11 @@ def wrangle_curr_logs():
     #set DateTime as index
     df = df.set_index('datetime')
     
+    #split time columns
+    df['year'] = df.index.year
+    df['month'] = df.index.month
+    df['day'] = df.index.day
+    df['hour'] = df.index.hour
+ 
     return df
     
